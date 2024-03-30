888c29aba35f0ff7a8d28cf3d4f3747b2a956af8
Yordanofff <ivailo.yordanov.ph@gmail.com>
2024-03-30 15:55:00 +0200

    project structure added

---
+++ src/test/java/com/project/Onlineshop/OnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
