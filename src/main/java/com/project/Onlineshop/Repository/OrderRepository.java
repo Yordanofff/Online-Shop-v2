24649c0e3214376c683894e0fea9c6cb36da7372
Gyokhan <gyokhan28@gmail.com>
2024-04-05 20:43:27 +0300

    Added form for user profile with orders.

--- src/main/java/com/project/Onlineshop/Repository/OrderRepository.java
+++ src/main/java/com/project/Onlineshop/Repository/OrderRepository.java
@@ -1,10 +1,13 @@
 package com.project.Onlineshop.Repository;
 
 import com.project.Onlineshop.Entity.Order;
+import com.project.Onlineshop.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findAllByUser_IdAndStatus_Id(Long userId, Long statusId);
+
+    List<Order> findByUserIdAndStatusName(Long userId, String statusName);
 }
